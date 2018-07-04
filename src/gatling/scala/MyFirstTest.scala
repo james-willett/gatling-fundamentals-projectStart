db0a4ee9ab57775a75486bfd531bdfe59036ded6
JWillet <james.willett@concur.com>
2018-07-04 18:51:17 +0100

    Removing template code from MyFirstTest.class

--- src/gatling/scala/MyFirstTest.scala
+++ src/gatling/scala/MyFirstTest.scala
@@ -4,20 +4,10 @@ import io.gatling.http.Predef._
 class MyFirstTest extends Simulation {
 
   // 1 Common HTTP Configuration
-  val httpConf = http
-    .baseURL("http://localhost:8080/app/")
-    .header("Accept", "application/json")
-    .proxy(Proxy("localhost", 8888).httpsPort(8888))
 
   // 2 Scenario Definition
-  val scn = scenario("My First Test")
-    .exec(http("Get All Games")
-      .get("videogames"))
 
   // 3 Load Scenario
-  setUp(
-    scn.inject(atOnceUsers(1))
-  ).protocols(httpConf)
 
 
 }
